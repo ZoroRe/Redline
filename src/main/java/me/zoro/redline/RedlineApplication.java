356c4be559ec4dd3685c07fb4f19f99082bbd4f6
luguanquan <757111858@qq.com>
2020-05-01 17:17:59 +0800

    init to spring cloud project

---
+++ src/main/java/me/zoro/redline/RedlineApplication.java
@@ -0,0 +1,16 @@
+package me.zoro.redline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author luguanquan
+ */
+@SpringBootApplication
+public class RedlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/zoro/redline/RedlineApplicationTests.java
@@ -0,0 +1,13 @@
+package me.zoro.redline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
